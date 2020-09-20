2d721b4f21e683f12e434b3b0a49cce5498385fb
Nattida Wong <nattidac.wong@gmail.com>
2020-09-20 02:15:29 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -54,7 +54,7 @@ def monopoly_with_fourth_tier
 		:railroads => {
 			:pieces => 4,
 			:names => {
-				:reading_railroad => {:names "mortgage_value", amount: "$100"},
+				:reading_railroad => {name: "mortgage_value", amount: "$100"},
 				:pennsylvania_railroad => {},
 				:b_and_o_railroad => {},
 				:shortline => {}
